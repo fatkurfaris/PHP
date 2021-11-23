7d6b207851a550441f6ff6ac9bcbc04c69d37f88
anak bontang <fatkurrizal12@gmail.com>
2021-11-24 01:34:52 +0700

    first commit

--- php/koneksi.php
+++ php/koneksi.php
@@ -1,7 +1,12 @@
 <?php 
-	$hostname ="ftpupload.net" ;
-	$username ="epiz_29113472" ;
-	$password ="YtALMVbEdg5Y" ;
+	// $hostname ="ftpupload.net" ;
+	// $username ="epiz_29113472" ;
+	// $password ="YtALMVbEdg5Y" ;
+	// $database ="itc";
+
+	$hostname ="localhost" ;
+	$username ="root" ;
+	$password = "" ;
 	$database ="itc";
 
 $konek=new mysqli($hostname,$username,$password,$database);
