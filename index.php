2c5500c37253fe692afbbbfc0238b0c073583b5d
fatkurfaris <@fatkurrizal12@gmail.com>
2022-08-06 22:50:48 +0700

    oop-class-object

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    
+class Coba{
+
+}
+
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+?>
+
+<html>
+    <body>
+<!-- <h1>Testing</h1> -->
+
+    </body>
+</html>
