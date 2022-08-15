75f6e6dc935e66a61b51a5135b6cc3cd7df2586f
fatkurfaris <@fatkurrizal12@gmail.com>
2022-08-15 20:56:56 +0700

    object type

--- index.php
+++ index.php
@@ -17,16 +17,26 @@ class Produk
 
     public function getLabel()
     {
-        return "$this->judul, $this->penulis, $this->penerbit, $this->harga";
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+class CetakInfoProduk
+{
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
     }
 }
 
 $produk1 = new Produk("Naruto", "Masashi kisimoto", "shonen jump", "Gratis");
 $produk2 = new Produk("Uncharted", "Neil druckman", "Sony computer", 222222);
-$produk3 = new Produk("gameing");
 
 echo "komik =" . $produk1->getLabel();
 echo "<br>";
 echo "Gambe = " . $produk2->getLabel();
 echo "<br>";
-var_dump($produk3);
+
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
