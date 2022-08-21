7675490f7ba5ed9cb1af7f82e242e6737f00f2f9
fatkurfaris <@fatkurrizal12@gmail.com>
2022-08-22 03:05:03 +0700

    oop namespace

--- App/Produk/Coba.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Coba
-{
-    public function __construct()
-    {
-        echo "ini adalah kelas coba";
-    }
-}
--- index.php
+++ index.php
@@ -2,16 +2,25 @@
 
 require_once 'App/Init.php';
 
-$produk1 = new Komik("Narutooo", "Masashi kisimoto", "shonen jump", "Gratis", 100);
-$produk2 = new Game("Uncharted", "Neil druckman", "Sony computer", 250000, 50);
+// $produk1 = new Komik("Narutooo", "Masashi kisimoto", "shonen jump", "Gratis", 100);
+// $produk2 = new Game("Uncharted", "Neil druckman", "Sony computer", 250000, 50);
 
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk($produk1);
-$cetakProduk->tambahProduk($produk2);
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
 
-echo $cetakProduk->cetak();
+// echo $cetakProduk->cetak();
 
+// echo "<hr>";
+
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+
+new ServiceUser();
 echo "<hr>";
+new ProdukUser();
 
-new Coba();
+// new App\Service\User();
+// echo "<hr>";
+// new App\Produk\User();
