8e8ef7534cde7d6033498bec0730d5c17033842a
fatkurfaris <@fatkurrizal12@gmail.com>
2022-08-22 02:00:09 +0700

    oop abstract

--- index.php
+++ index.php
@@ -1,34 +1,186 @@
 <?php
 
-define('NAMA', 'FATKUR RIZAL ROCHMADINA PUTRA');
-
-echo NAMA;
-
-const UMUR = 21;
-echo UMUR, "<hr>";
-
-
-
-// class Coba
-// {
-//     const NAMA = 'FATKUR ';
-// }
-
-// echo Coba::NAMA;
-
-// echo __FILE__;
-
-// function Coba()
-// {
-//     return __FUNCTION__;
-// }
-
-// echo Coba();
-
-class Coba
+abstract class Produk
 {
-    public $kelas = __CLASS__;
+    private $judul,
+        $penulis,
+        $penerbit,
+        $harga,
+        $diskon = 0;
+
+    public function __construct(
+        $judul = "juudul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0
+
+    ) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function setJudul($judul)
+    {
+        if (!is_string($judul)) {
+            throw new Exception("judul harus string");
+        }
+        $this->judul = $judul;
+    }
+
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function setPenulis($penulis)
+    {
+        $this->judul = $penulis;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    abstract public function getInfoProduk();
+
+    public function getInfo()
+    {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ";
+
+        return $str;
+    }
+
+
+
+    public function getDiskon()
+    {
+        return $this->diskon;
+    }
+
+    public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
+    public function setHarga($harga)
+    {
+        $this->harga = $harga;
+    }
+
+    public function getHarga()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
 }
 
-$obj = new Coba;
-echo $obj->kelas;
+class Komik extends Produk
+{
+    public $jmlHalaman;
+
+    public function __construct($judul = "juudul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0)
+    {
+        parent::__construct(
+            $judul,
+            $penulis,
+            $penerbit,
+            $harga
+        );
+
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Komik : " . $this->getInfo() . " - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+    public $waktuMain;
+
+    public function __construct($judul = "juudul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
+    {
+        parent::__construct(
+            $judul,
+            $penulis,
+            $penerbit,
+            $harga
+        );
+
+        $this->waktuMain = $waktuMain;
+    }
+
+
+    public function getInfoProduk()
+    {
+        $str = "Game : " . $this->getInfo() . " - {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+class CetakInfoProduk
+{
+    public $daftarProduk = array();
+
+
+    public function tambahProduk(Produk $produk)
+    {
+        $this->daftarProduk[] = $produk;
+    }
+
+
+    public function cetak()
+    {
+        $str = "Daftar Produk : <br>";
+
+        foreach ($this->daftarProduk as $p) {
+            $str .= "-{$p->getInfoProduk()} <br>";
+        }
+        return $str;
+    }
+}
+
+
+$produk1 = new Komik("Narutooo", "Masashi kisimoto", "shonen jump", "Gratis", 100);
+$produk2 = new Game("Uncharted", "Neil druckman", "Sony computer", 250000, 50);
+
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+
+echo $cetakProduk->cetak();
+
+
+// echo $produk1->getInfoProduk();
+// echo "<br>";
+// echo $produk2->getInfoProduk();
+// echo "<hr>";
+
+// $produk2->setDiskon(50);
+// echo $produk2->getHarga();
+// echo "<hr>";
+
+// $produk1->setJudul("judulBaru");
+// echo $produk1->getJudul();
+// echo $produk1->getPenulis();
